4d6e21cccb695a37cacbf13ff4bc0ab943c5869f
Xu ran Bai <v-xuranbai@microsoft.com>
2020-12-22 17:52:53 +0800

    as normal by this

--- sql.sql
+++ sql.sql
@@ -19,4 +19,77 @@ At new year,you should study new something.
 采用一个spring写一个程序论坛网站 或者一个电商平台 
 
 下周开始复习Java基础知识和掌握REDIS，写一个自己的开源项目
-然后掌握spring 
+然后掌握spring
+# 查找人数最多的三个城市 
+country 
+id name location
+select location ,count(id) as number from country order by number desc limit 3 ;
+
+# 找出每一科成绩最高的人
+study 
+id name object sorce
+select name,object,max(sorce) as '最高分' from study group by object;
+
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+      # Write your MySQL query statement below.
+      select Salary 
+      from Employee
+      group by Salary ,
+      order by Salary desc,
+      limit N,1; 
+  );
+END
+
+select Department.name as Department,Employee.name as Employee,max(Salary) as  from Employee,Department group by DepartmentID
+SELECT 
+    Department.NAME AS 'DEPARTMENT',
+    Employee.NAME AS 'EMPLOYEE',
+    Salary
+FROM EMPLOYEE
+        JOIN 
+    Department ON Employee.DepartmentID = Department.id
+WHERE
+    (Employee.DepartmentID,Salary) IN 
+    (SELECT
+        DEPARTMENTID,MAX(Salary)
+        FROM Employee
+        group BY DEPARTMENTID
+    )
+;
+
+SELECT 
+    DEPARTMENT.NAME AS 'DEPARTMENT',
+    Employee.NAME AS 'EMPLOYEE',
+    Salary
+FroM 
+    Employee
+        JOIN
+    Department ON Employee.DepartmentID = Department.id
+WHERE
+    (EMPLOYEE.DEPARTMENTID,Salary) IN
+    (   SELECT
+            DEPARTMENTID,MAX(Salary)
+        FROM 
+            Employee
+        group BY DEPARTMENTID
+    )
+;
+SELECT
+    Department.name AS 'Department',
+    Employee.name AS 'Employee',
+    Salary
+FROM
+    Employee
+        JOIN
+    Department ON Employee.DepartmentId = Department.Id
+WHERE
+    (Employee.DepartmentId , Salary) IN
+    (   SELECT
+            DepartmentId, MAX(Salary)
+        FROM
+            Employee
+        GROUP BY DepartmentId
+	)
+;
