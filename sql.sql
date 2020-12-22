ef898e86687cda102a0256374cafe61b376fa3c8
Xu ran Bai <v-xuranbai@microsoft.com>
2020-12-22 16:08:21 +0800

    test

--- sql.sql
+++ sql.sql
@@ -5,5 +5,16 @@
 每天一道SQL算法题一道 数据结构算法题
 
 周末之前搞定所有的计算机网络和操作系统的相关知识
+
+2020完全搞定应该搞定的知识
+彻底搞定关于Java的所有知识 for  one week time 
+还有数据结构和算法
+
+At new year,you should study new something.
+
+当然这些知识一个也不能落下
+
+每次面试之前，关于每一类的面试题至少写一个
+
 下周开始复习Java基础知识和掌握REDIS，写一个自己的开源项目
 然后掌握spring 
