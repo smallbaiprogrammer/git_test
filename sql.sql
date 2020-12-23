bbc80f4441c48b02d0115033530c70e4a489e01d
Xu ran Bai <v-xuranbai@microsoft.com>
2020-12-23 11:19:31 +0800

    test

--- sql.sql
+++ sql.sql
@@ -23,7 +23,8 @@ At new year,you should study new something.
 # 查找人数最多的三个城市 
 country 
 id name location
-select location ,count(id) as number from country order by number desc limit 3 ;
+select location ,count(id) as number from country order by number desc limit 0,3 ;
+SELECT location ,count()
 
 # 找出每一科成绩最高的人
 study 
@@ -105,8 +106,25 @@ select Person.FirstName ,
 ;
 
 # 关于 MySQL 的 知识今天全部搞定
+
+# mysql 数据库的主从同步
+# mysql 一台主服务器，多台从服务器，主服务器修改日志的二进制文件，从服务器通过日志的二进制文件来进行相应的操作
+MVCC 乐观锁的实现 实现了 读-写 写-读 非阻塞 只有在写-写的时候才会进行阻塞
+当事务更新数据的时候，不会直接覆盖以前的数据，而是生成一个新的版本的数据，因此每一条数据都会有多个版本进行储存
+这种方式其实与JAVA的CopyOnWriteHashmap 类似 这个版本一般是事务结束的时间戳 或者是 随事务增长的id
+@https://yq.aliyun.com/articles/283418
 # 关于Mysql 的 知识上午看 
 # 下午练习
 # 晚上总结一下
 
-# 
+# CLASS ID NAME OBJECT SORCE 
+SELECT MAX(SORCE) AS 'MAX SORCE' FROM CLASS GROUP BY OBJECT; 
+
+SELECT DEPARTMENT.NAME AS 'DEPARTMENT' , EMPLOYEE.NAME AS 'EMPLOYEE' ,Salary
+FROM DEPARTMENT JOIN EMPLOYEE ON DEPARTMENT.ID = EMPLOYEE.DEPARTMENTID
+WHERE (DEPARTMENT.id, Salary) IN (SELECT DEPARTMENTID , MAX(Salary) FROM EMPLOYEE GROUP BY DEPARTMENTID)
+; 
+
+
+# 关于 spring 框架 
+@ https://www.w3cschool.cn/wkspring/ 
